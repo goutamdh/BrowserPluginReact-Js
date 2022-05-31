e1c5c638bd9a992affb53eb507ac8b83ea67c2b2
Goutam Kumar Dhandh <gdhandh@gmail.com>
2022-05-31 22:42:37 +0530

    Weather app

---
+++ src/utils/api.ts
@@ -0,0 +1,45 @@
+const OPEN_WEATHER_API_KEY = '<INSERT OPEN WEATHER API KEY>'
+
+export interface OpenWeatherData {
+  name: string
+  main: {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  weather: {
+    description: string
+    icon: string
+    id: number
+    main: string
+  }[]
+  wind: {
+    deg: number
+    speed: number
+  }
+}
+
+export type OpenWeatherTempScale = 'metric' | 'imperial'
+
+export async function fetchOpenWeatherData(
+  city: string,
+  tempScale: OpenWeatherTempScale
+): Promise<OpenWeatherData> {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${tempScale}&appid=${OPEN_WEATHER_API_KEY}`
+  )
+
+  if (!res.ok) {
+    throw new Error('City not found')
+  }
+
+  const data: OpenWeatherData = await res.json()
+  return data
+}
+
+export function getWeatherIconSrc(iconCode: string) {
+  return `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+}
